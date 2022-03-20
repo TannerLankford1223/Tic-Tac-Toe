d1330744192ec293e9c1e84532aaab59698001e2
TannerLankford1223 <T.Lankford730@gmail.com>
2022-03-20 04:21:13 -0500

    Implement functionality to place marker on board

--- resources/ts/build/script.js
+++ resources/ts/build/script.js
@@ -1,1 +1,60 @@
 "use strict";
+const MARK = 'O';
+const gameBoard = (() => {
+    let tiles = ['', '', '', '', '', '', '', '', '',];
+    const placeMark = (index, playerMark) => {
+        if (isEmpty(index)) {
+            tiles[index] = playerMark;
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    const isEmpty = (index) => {
+        return tiles[index] === '';
+    };
+    return {
+        tiles,
+        placeMark,
+        isEmpty
+    };
+})();
+const displayController = (() => {
+    let playerMark = MARK;
+    const cells = document.querySelectorAll('.board-cell');
+    const restartButton = document.querySelector('.restart');
+    const cellFields = document.querySelectorAll('.marker');
+    // Adds the players marker to the cell field
+    const populateCell = (e) => {
+        if (e !== null) {
+            let index = Number(e.target.dataset.cell);
+            console.log("clicked on index: " + index);
+            const cellField = e.target.querySelector('.marker');
+            if (gameBoard.placeMark(index, playerMark)) {
+                cellField.innerText = playerMark;
+            }
+        }
+        else {
+            console.log("Event was null");
+        }
+    };
+    const restart = (e) => {
+        cellFields.forEach((cellField) => {
+            cellField.innerText = '';
+        });
+    };
+    // Adds event listeners before parent module is initialized
+    const _init = (() => {
+        cells.forEach((cell) => {
+            cell.addEventListener('click', populateCell);
+        });
+        restartButton.addEventListener('click', restart);
+    })();
+    return {
+        playerMark,
+        cells,
+        populateCell,
+        restart
+    };
+})();
---
+++ resources/ts/script.ts
@@ -0,0 +1,71 @@
+const MARK = 'O';
+
+const gameBoard = (() => {
+    let tiles: string[] = ['', '', '', '', '', '', '', '', '',];
+
+    const placeMark = (index: number, playerMark: string) => {
+        if (isEmpty(index)) {
+            tiles[index] = playerMark;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    const isEmpty = (index: number) => {
+        return tiles[index] === '';
+    };
+
+    return {
+        tiles,
+        placeMark,
+        isEmpty
+    }
+})();
+
+const displayController = (() => {
+    let playerMark: string = MARK;
+    const cells: NodeListOf<Element> = document.querySelectorAll('.board-cell');
+    const restartButton = document.querySelector('.restart');
+    const cellFields: NodeListOf<Element> = document.querySelectorAll('.marker');
+
+    // Adds the players marker to the cell field
+    const populateCell = (e: Event) => {
+        if (e !== null) {
+            let index: number = Number((e.target as HTMLElement).dataset.cell);
+            console.log("clicked on index: " + index);
+
+            const cellField = (e.target as HTMLElement).querySelector('.marker');
+
+            if (gameBoard.placeMark(index, playerMark)) {
+                (cellField as HTMLElement).innerText = playerMark;
+            }
+        } else {
+            console.log("Event was null");
+        }
+    }
+
+    const restart = (e: Event) => {
+        cellFields.forEach((cellField : Element) => {
+            (cellField as HTMLElement).innerText = '';
+        });
+    }
+
+    // Adds event listeners before parent module is initialized
+    const _init = (() => {
+        cells.forEach((cell: Element) => {
+            cell.addEventListener('click', populateCell);
+        });
+
+        restartButton!.addEventListener('click', restart);
+    })();
+
+    return {
+        playerMark,
+        cells,
+        populateCell,
+        restart
+    }
+})();
+
+
